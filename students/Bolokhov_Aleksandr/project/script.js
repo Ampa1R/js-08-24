b152c7c249b1133973ab9765cd3caece413e9c2d
Aleksandr Bolokhov <bolohov@inbox.ru>
2020-09-03 12:32:21 +0300

    update

--- students/Bolokhov_Aleksandr/project/script.js
+++ students/Bolokhov_Aleksandr/project/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/* Начало начального кода.
 class GoodsItem {
   constructor(title, price) {
     this.title = title;
@@ -29,7 +29,7 @@ class GoodsList {
     });
     document.querySelector('.goods-list').innerHTML = listHtml;
   }
-  
+
   goodsPriceSum() {
    let result = null;
    this.goods.forEach(good => {
@@ -98,3 +98,64 @@ const list = new GoodsList();
 list.fetchGoods();
 list.render();
 list.goodsPriceSum();
+Конец начального кода*/
+
+
+function makeGETRequest(url, callback) {
+  let xhr;
+
+  if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest();
+  } else if (window.ActiveXObject) { 
+    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      callback(xhr.responseText);
+    }
+  }
+
+  xhr.open('GET', url, true);
+  xhr.send();
+}
+
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+
+  class GoodsItem {
+      render() {
+        return `<div class="goods-item"><h3>${this.product_name}</h3><p>${this.price}</p></div>`;
+      }
+    }
+    
+    class GoodsList {
+
+      constructor () {
+      this.goods = [];
+      }
+
+      fetchGoods(cd) {
+        makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
+          this.goods = JSON.parse(goods);
+          console.log(this.goods);
+          cd();
+        })
+      }
+      
+
+      render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+          const goodItem = new GoodsItem(good.product_name, good.price);
+          listHtml += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = listHtml;
+      }
+    }
+
+  const list = new GoodsList();
+  list.fetchGoods(() => {
+    list.render();
+  });
+  
