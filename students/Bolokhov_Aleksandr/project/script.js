abf36539e9b6739957edfc0fef239b833b200097
Aleksandr Bolokhov <bolohov@inbox.ru>
2020-09-09 10:54:54 +0300

    update project after lesson 5

--- students/Bolokhov_Aleksandr/project/script.js
+++ students/Bolokhov_Aleksandr/project/script.js
@@ -1,35 +1,38 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-function sendReqest(url) {
-  return new Promise ((resolve, reject) => {
+function sendRequest(url) {
+  return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest;
+  
     xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          resolve(JSON.parse(xhr.responseText));
-        } else {
-          reject(xhr.responseText);
+        if (xhr.readyState === 4) {
+
+          if (xhr.status === 200) {
+            resolve(JSON.parse(xhr.responseText));
+          } else {
+            reject(xhr.responseText);
+          }
+
         }
-      }
     }
 
-    xhr.open("GET", `${API}${url}`, true);
-
+    xhr.open('GET', `${API}${url}`, true);
+  
     xhr.send();
   });
-}  
-
+}
+/*
 class GoodsItem {
-  constructor({id_product, product_name, price }) {
+  constructor({ id_product, product_name, price }) {
     this.id = id_product;
-    this.product_name = product_name;
+    this.title = product_name;
     this.price = price;
   }
 
   render() {
     return `
       <div class="goods-item" data-id="${this.id}">
-        <h3>${this.product_name}</h3>
+        <h3>${this.title}</h3>
         <p>${this.price}</p>
         <button name="add-to-basket">Buy</button>
       </div>
@@ -43,46 +46,47 @@ class GoodsList {
     this.goods = [];
     this.filteredGoods = [];
     this.fetchGoods()
-    .then(() => {
-      this.render();
+      .then(() => {
+        this.render();
+      })
+      .catch((err) => {
+        console.log('[ERROR]', err);
+      });
+
+    document.querySelector('.goods-list').addEventListener('click', (event) => {
+      if (event.target.name === 'add-to-basket') {
+        const id = event.target.parentElement.dataset.id;
+        const item = this.goods.find((goodsItem) => goodsItem.id_product === parseInt(id));
+        this.basket.addItem(item);
+      }
     });
 
-  document.querySelector(".goods-list").addEventListener("click", (event) => {
-    if (event.target.name === "add-to-basket") {
-      const id = event.target.parentElement.dataset.id;
-      const item = this.goods.find((goodsItem) => goodsItem.id_product === Number(id));
-      this.basket.addItem(item);
-    }
-  });
-
-  document.querySelector(".search").addEventListener("input", (event) => {
-    this.filterGoods(event.target.value);
-  });
-}
+    document.querySelector('.search').addEventListener('input', (event) => {
+      this.filterGoods(event.target.value);
+    });
+  }
 
   fetchGoods() {
-    return new Promise ((resolve, reject) => {
-    sendReqest(`/catalogData.json`)
-    .then((goods) => {
-      this.goods = goods;
-      this.filteredGoods = goods;
-      resolve();
-    })
-    .catch((err) => {reject(err);
+    return new Promise((resolve, reject) => {
+      sendRequest('/catalogData.json')
+        .then((goods) => {
+          this.goods = goods;
+          this.filteredGoods = goods;
+          resolve();
+        })
+        .catch((err) => {
+          reject(err);
+        });
     });
-   });
   }
-  
+
   total() {
     return this.goods.reduce((acc, cur) => acc + cur.price, 0);
   }
 
   filterGoods(value) {
     const regexp = new RegExp(value, 'i');
-    this.filteredGoods = this.goods.filter((item) => { 
-      regexp.test(item.product_name);
-      console.log(this.filteredGoods);
-    });
+    this.filteredGoods = this.goods.filter(item => regexp.test(item.product_name));
     this.render();
   }
 
@@ -95,46 +99,102 @@ class GoodsList {
   }
 }
 
-
-
 class Basket {
-constructor() {
-  this.goods = [];
-}
+  constructor() {
+    this.goods = [];
+  }
 
-fetchGoods() {
+  fetchGoods() {
 
-}
+  }
 
-addItem(item) {
-  const itemIndex = this.goods.findIndex((goodsItem) => goodsItem.id_product === item.id_product);
-  if (itemIndex !== -1) {
-    this.goods[itemIndex].quantity++;
-    
+  addItem(item) {
+    const itemIndex = this.goods.findIndex((goodsItem) => goodsItem.id_product === item.id_product);
+    if (itemIndex !== -1) {
+      this.goods[itemIndex].quantity++;
+    } else {
+      this.goods.push({ ...item, quantity: 1 });
+    }
+    console.log(this.goods);
+  }
+
+  removeItem(id) {
+    const itemIndex = this.goods.findIndex((goodsItem) => goodsItem.id_product === id);
+    if (itemIndex !== -1) {
+      this.goods.splice(itemIndex, 1);
+    }
+  }
+
+  getBasketItems() {
+    return this.goods;
+  }
+
+  total() {
+
+  }
+
+  render() {
+
   }
 }
 
-remoteItem(id) {
-  const itemIndex = this.goods.findIndex((goodsItem) => goodsItem.id_product === id);
-  if (itemIndex !== -1) {
-    this.goods.splice(itemIndex, 1);
+class BasketItem {
+  constructor(item, basket) {
+    this.item = item;
+    this.basket = basket;
   }
-}
 
-total() {
+  addItem() {
+    this.basket.addItem(this.item.id);
+  }
 
-}
+  removeItem() {
+    this.basket.removeItem(this.item.id);
+  }
 
-render() {
+  add() {
+    this.item.quantity += 1;
+  }
 
-}
+  render() {
 
-
+  }
 }
 
 const basket = new Basket();
 const goodsList = new GoodsList(basket);
+*/
 
-class BasketItem {
-
-  }
+const app = new Vue({
+  el: '#app',
+  data: {
+    goods: [],
+    searchText: '',
+  },
+  created() {
+    this.fetchGoods();
+  },
+  computed: {
+    filteredGoods() {
+      const regexp = new RegExp(this.searchText, 'i');
+      return this.goods.filter(item => regexp.test(item.product_name));
+    },
+    total() {
+      return this.goods.reduce((acc, cur) => acc + cur.price, 0);
+    },
+  },
+  methods: {
+    fetchGoods() {
+      return new Promise((resolve, reject) => {
+        sendRequest('/catalogData.json')
+          .then((goods) => {
+            this.goods = goods;
+            resolve();
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+  },
+});
