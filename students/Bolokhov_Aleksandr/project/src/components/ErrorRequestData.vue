0ca6edcdd205d135abdb9e7667ed045dc5d72e09
Aleksandr Bolokhov <bolohov@inbox.ru>
2020-09-17 11:46:26 +0300

    homework-6

---
+++ students/Bolokhov_Aleksandr/project/src/components/ErrorRequestData.vue
@@ -0,0 +1,22 @@
+<template>
+<footer>  
+<h1 v-if="errorText">{{textError}}</h1>
+</footer>    
+</template>
+
+<script>
+export default {
+    name: "ErrorRequestData",
+    props: ['errorText'],
+    data() {
+        return {
+            textError: "Что-то пошло не так!",
+        };
+    },   
+}
+
+</script>
+
+<style>
+/* */
+</style>
